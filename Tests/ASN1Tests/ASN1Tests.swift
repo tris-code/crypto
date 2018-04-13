0e681f2617e54d2c7192b7da1f0f3bc9b86ada05
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-04-13 20:31:03 +0000

    [ASN.1] Rename 'ber' tag to 'endOfContent'

--- Tests/ASN1Tests/ASN1Tests.swift
+++ Tests/ASN1Tests/ASN1Tests.swift
@@ -17,7 +17,7 @@ class ASN1Tests: TestCase {
         let identifier1 = ASN1.Identifier(
             isConstructed: true,
             class: .contextSpecific,
-            tag: .ber)
+            tag: .endOfContent)
 
         let identifier2 = ASN1.Identifier(
             isConstructed: true,
