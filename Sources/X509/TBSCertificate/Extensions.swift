75720eeb17523bd1c040d74e2a73e098c05a2c4d
Tony Freeman <tonyfreeman@users.noreply.github.com>
2018-12-12 12:09:41 +0000

    [X509] Decode Netscape CertificateType extension

--- Sources/X509/TBSCertificate/Extensions.swift
+++ Sources/X509/TBSCertificate/Extensions.swift
@@ -28,6 +28,12 @@ public struct Extension: Equatable {
     case certificatePolicies(CertificatePolicies)
     // id-pe-*
     case authorityInfoAccessMethod(AuthorityInfoAccess)
+    // netscape
+    case netscape(Netscape)
+    }
+
+    public enum Netscape: Equatable {
+        case certificateType(CertificateType)
     }
 }
 
@@ -96,6 +102,8 @@ extension Extension {
             self.value = .authorityInfoAccessMethod(try .init(from: value))
         case .certificateExtension(.some(.certificatePolicies)):
             self.value = .certificatePolicies(try .init(from: value))
+        case .netscape(.some(.certificateExtension(.certificateType))):
+            self.value = .netscape(.certificateType(try .init(from: value)))
         default:
             throw Error.unimplemented(asn1)
         }
